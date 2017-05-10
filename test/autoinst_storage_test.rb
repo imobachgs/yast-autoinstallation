10ba8869a5878a1271e0e8a2ea079ad54a2754ae
Imobach González Sosa <igonzalezsosa@suse.com>
2017-05-10 15:32:27 +0100

    Refactor proposal settings support

--- test/autoinst_storage_test.rb
+++ test/autoinst_storage_test.rb
@@ -8,7 +8,7 @@ describe Yast::AutoinstStorage do
 
   describe "#Import" do
     let(:profile) do
-      { "proposal" => {} }
+      { "proposal" => { "use_lvm" => true } }
     end
 
     let(:proposal_settings_class) { double("ProposalSettingsClass") }
@@ -24,14 +24,25 @@ describe Yast::AutoinstStorage do
       stub_const("Y2Storage::ProposalSettings", proposal_settings_class)
       stub_const("Y2Storage::Proposal", proposal_class)
       stub_const("Y2Storage::StorageManager", storage_manager_class)
-      allow(proposal_settings_class).to receive(:new).and_return(proposal_settings)
+      allow(proposal_settings_class).to receive(:new_for_current_product).and_return(proposal_settings)
       allow(proposal_class).to receive(:new).with(settings: proposal_settings)
         .and_return(proposal)
+      allow(storage_manager).to receive(:proposal=)
+      allow(proposal_settings).to receive(:use_lvm=)
     end
 
     it "sets the proposal" do
       expect(storage_manager).to receive(:proposal=).with(proposal)
       subject.Import(profile)
+    end
+
+    it "builds a proposal with given options" do
+      expect(proposal_settings).to receive(:use_lvm=).with(true)
+      subject.Import(profile)
+    end
+
+    it "returns true" do
+      expect(subject.Import(profile)).to eq(true)
     end
 
     context "if proposal fails" do
@@ -46,23 +57,42 @@ describe Yast::AutoinstStorage do
     end
 
     context "when profile is nil" do
+      let(:profile) { nil }
+
       it "creates a proposal with default settings" do
         expect(storage_manager).to receive(:proposal=).with(proposal)
-        subject.Import(nil)
+        subject.Import(profile)
       end
     end
 
     context "when profile is empty" do
+      let(:profile) { {} }
+
       it "creates a proposal with default settings" do
         expect(storage_manager).to receive(:proposal=).with(proposal)
-        subject.Import({})
+        subject.Import(profile)
       end
     end
 
-    context "when profile is not empty but does not contains proposal settings" do
-      it "creates a proposal with not settings" do
+    context "when profile contains an empty set of partitions" do
+      let(:profile) { [] }
+
+      it "builds a proposal" do
+        expect(storage_manager).to receive(:proposal=).with(proposal)
+        subject.Import(profile)
+      end
+    end
+
+    context "when profile contains a set of partitions" do
+      let(:profile) { [{"device" => "/dev/sda"}] }
+
+      it "does not build a proposal" do
         expect(proposal_settings_class).to_not receive(:new)
-        subject.Import("partitioning" => [])
+        subject.Import(profile)
+      end
+
+      it "returns false" do
+        expect(subject.Import(profile)).to eq(false)
       end
     end
   end
